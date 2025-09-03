0ed036afa3a2e3452082dae9830ce04ade32c036
bxx808 <aleksandrovvlad10@gmail.com>
2025-09-03 12:59:05 +0300

    сreated sections reviews

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,5 @@ import '@fontsource/inter';
 import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
 import '@fontsource/inter/400.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
